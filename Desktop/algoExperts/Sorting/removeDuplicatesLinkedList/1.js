19580c3d4ad39340850c7b57802e263bf4045024
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-07-27 20:42:15 -0400

    remove duplicates

---
+++ Desktop/algoExperts/Sorting/removeDuplicatesLinkedList/1.js
@@ -0,0 +1,20 @@
+class LinkedList {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+function removeDuplicatesFromLinkedList(linkedList) {
+    let currentNode = linkedList;
+    while (currentNode !== null) {
+        let nextDistinctNode = currentNode.next;
+        while (nextDistinctNode !== null && nextDistinctNode.value === currentNode.value) {
+            nextDistinctNode = nextDistinctNode.next;
+        }
+
+        currentNode.next = nextDistinctNode;
+        currentNode = nextDistinctNode.next;
+    }
+    return linkedList
+}
